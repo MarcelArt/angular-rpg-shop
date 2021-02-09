4215e7b375e6cc147a114fbbd9d2a1d467cba54f
MarcelArt <marcel.aritonang@gmail.com>
2021-02-09 09:51:27 +0700

    added process.env.port

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/dist/challenge1/index.html');
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
